06fd08f3c4e69ff2b29e61ea3e50ba4519472f5d
Ipsis <mihan_88@bk.ru>
2018-03-02 17:39:29 +0300

    Update mtk_cam.cpp

--- libmtk_symbols/mtk_cam.cpp
+++ libmtk_symbols/mtk_cam.cpp
@@ -6,3 +6,19 @@ extern "C" void _ZN9NSCamShot10IMultiShot14createInstanceEN5NSCam9EShotModeEPKc(
 extern "C" void _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb() { }
 
 
+extern "C" void _ZN7android18BufferItemConsumer13releaseBufferERKNS_22IGraphicBufferConsumer10BufferItemERKNS_2spINS_5FenceEEE() { }
+extern "C" void _ZN7android5Fence8NO_FENCEE() { }
+extern "C" void _ZThn16_NK5NSCam15EffectHalClient21onInputFrameProcessedEPKNS_16IEffectHalClientEN7android2spINS_15BasicParametersEEES6_() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_18BufferItemConsumerEEEE7do_copyEPvPKvj() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_18BufferItemConsumerEEEE8do_splatEPvPKvj() { }
+extern "C" void _ZN7android7RefBase10renameRefsEjRKNS_16ReferenceRenamerE() { }
+extern "C" void _ZTVZN7android14ReferenceMover15move_referencesIN5NSCam15BasicParametersEEEvPNS_2spIT_EEPKS6_jE7Renamer() { }
+extern "C" void _ZNK7android6VectorINS_2spIN5NSCam15BasicParametersEEEE15do_move_forwardEPvPKvj() { }
+extern "C" void _ZNK7android6VectorINS_2spIN5NSCam15BasicParametersEEEE16do_move_backwardEPvPKvj() { }
+extern "C" void _ZTVZN7android14ReferenceMover15move_referencesINS_13GraphicBufferEEEvPNS_2spIT_EEPKS5_jE7Renamer() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_13GraphicBufferEEEE15do_move_forwardEPvPKvj() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_13GraphicBufferEEEE16do_move_backwardEPvPKvj() { }
+extern "C" void _ZN7android2spINS_12ConsumerBase22FrameAvailableListenerEEC2ERKS3_() { }
+extern "C" void _ZN7android2spINS_12ConsumerBase22FrameAvailableListenerEEC1ERKS3_() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_12ConsumerBase22FrameAvailableListenerEEEE7do_copyEPvPKvj() { }
+extern "C" void _ZNK7android6VectorINS_2spINS_12ConsumerBase22FrameAvailableListenerEEEE8do_splatEPvPKvj() { }
