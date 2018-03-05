cd725d9bf9e3e990caac52af1d292626a3be6752
Ipsis <mihan_88@bk.ru>
2018-03-05 14:47:47 +0300

    Update mtk_cam.cpp

--- libmtk_symbols/mtk_cam.cpp
+++ libmtk_symbols/mtk_cam.cpp
@@ -6,4 +6,8 @@ extern "C" void _ZN9NSCamShot10IMultiShot14createInstanceEN5NSCam9EShotModeEPKc(
 extern "C" void _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb() { }
 
 
-
+extern "C" void _ZN7android8String165setToEPKtj() { }
+extern "C" void _Z24openssl_generate_keypairPK16keymaster_device19keymaster_keypair_tPKvPPhPj() { }
+extern "C" void _ZN9MTKM4UDrv19m4u_enable_m4u_funcEi() { }
+extern "C" void CRYPTO_free() { }
+extern "C" void _ZN7android13AwesomePlayer24mtk_omx_get_current_timeEPx() { }
